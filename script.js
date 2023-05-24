5cc948a1f0de0646f0f9dbf9d27bbd77b150dae0
mh07607 <mh07607@st.habib.edu.pk>
2023-05-25 00:07:06 +0500

    crude implementation

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 
 const moves = ['rock', 'paper', 'scissors'];
+var rounds = 0;
+var playerScore = 0;
+var computerScore = 0;
 
 function getComputerChoice(){
     let index = Math.floor(Math.random()*3);
@@ -8,60 +11,71 @@ function getComputerChoice(){
 }
 
 function playRound(playerselection, computerselection){
+    rounds = rounds+1;
     playerselection = playerselection.toLowerCase();
     if(playerselection == computerselection){
-        return "It's a tie!"
+        return "It's a tie!";
     }
 
     else if(playerselection == "rock" && computerselection == "paper"){
-        return "You Lose! Paper beats Rock"
+        computerScore = computerScore+1;
+        return "You Lose! Paper beats Rock";
     }else if(playerselection == "scissors" && computerselection == "paper"){
-        return "You Win! Scissors beat Paper"
+        playerScore = playerScore+1;
+        return "You Win! Scissors beat Paper";
     }
     
     else if(playerselection == "paper" && computerselection == "rock"){
-        return "You Win! Paper beats Rock"
+        playerScore = playerScore+1;
+        return "You Win! Paper beats Rock";
     }else if(playerselection == "scissors" && computerselection == "rock"){
-        return "You Lose! Rock beats Scissors"
+        computerScore = computerScore+1;
+        return "You Lose! Rock beats Scissors";
     }
 
     else if(playerselection == "rock" && computerselection == "scissors"){
-        return "You Win! Rock beats Scissors"
+        playerScore = playerScore+1;
+        return "You Win! Rock beats Scissors";
     }else if(playerselection == "paper" && computerselection == "scissors"){
-        return "You Lose! Scissors beat Paper"
+        computerScore = computerScore+1;
+        return "You Lose! Scissors beat Paper";
     }    
 }
 
 function game(){
-    let playerwins = 0;
-    let computerwins = 0;
-    for(let i = 0; i < 5; i++)
-    {
-        let playerselection = prompt("Rock, paper or scissors?");
-        let computerselection = getComputerChoice();
+    //getting componenets from html
+    const buttons = document.querySelectorAll("button");
+    const roundStatus = document.querySelector(".round_status");
+    const ui__playerScore = document.querySelector("#playerScore");
+    const ui__computerScore = document.querySelector("#computerScore");
+    const ui_winner = document.querySelector("#winner");
 
-        round_result = playRound(playerselection, computerselection);
-
-        console.log(playerselection);
-        console.log(computerselection);
-        console.log(round_result);
-
-        if(round_result[4] == 'W'){
-            playerwins = playerwins + 1;
-        } else if (round_result[4] == 'L'){
-            computerwins= computerwins + 1;
-        }
-    }
-
-    if(playerwins > computerwins){
-        console.log("You Win the game!");
-    } else if(playerwins < computerwins){
-        console.log("You Lose the game!");
-    }
-    else{
-        console.log("The game's a Draw!");
-    }
-
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            //simulating one round by PlayRound function; 
+            //passing in button text because text of button tells us the corresponding move of the player. 
+            roundStatus.textContent = "Round:" + (rounds+1) + 
+                                      playRound(button.textContent, getComputerChoice());
+            ui__computerScore.textContent = computerScore;
+            ui__playerScore.textContent = playerScore;
+            if(rounds > 4){
+                rounds = 0;
+                if(playerScore > computerScore){
+                    ui_winner.textContent = "Player wins";
+                } else if(playerScore < computerScore){
+                    ui_winner.textContent = "Computer wins";
+                } else {
+                    ui_winner.textContent = "It's a tie";
+                }
+                playerScore = 0;
+                computerScore = 0;
+                ui__computerScore.textContent = 0;
+                ui__playerScore.textContent = 0;
+            }
+            
+        });
+    });
+    
 }
 
-game()
+game();
